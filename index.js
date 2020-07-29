dcc2d8f1e1273e380e87ad608e55211751aa6247
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-29 15:03:00 -0400

    Done.

--- index.js
+++ index.js
@@ -19,3 +19,20 @@ function matchName(drivers, string){
     });
     return arr
 }
+
+// Flatiron solution:
+// function findMatching(source, sought) {
+//     return source.filter( possibleMatch =>
+//       possibleMatch.toLowerCase() === sought.toLowerCase()
+//     )
+//   }
+  
+//   function fuzzyMatch(source, testString) {
+//     return source.filter( possibleMatch =>
+//       possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     )
+//   }
+  
+//   function matchName(source, soughtName) {
+//     return source.filter( record => record.name === soughtName)
+//   }
