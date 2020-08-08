b37ba0221f276da04fced5f2b1fbf20d8b71b7d5
LeonorPDX <leonoracolbert@gmail.com>
2020-08-08 11:56:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 // Code your solution here
 function findMatching(arr, string) {
-    let matches = arr.filter(obj => {
-        return obj.toUpperCase() == string.toUpperCase()
+    return arr.filter(obj => {
+        return obj.toUpperCase() === string.toUpperCase()
     });
-    return matches
 }
 
 function fuzzyMatch(arr, string) {
-    let matches = arr.filter(obj => {
+    return arr.filter(obj => {
         return obj.startsWith(string)
     });
-    return matches
 }
 
 function matchName(arr, string) {
-    let matches = arr.filter(obj => {
+    return arr.filter(obj => {
         return obj.name === string
     });
-    return matches
 }
