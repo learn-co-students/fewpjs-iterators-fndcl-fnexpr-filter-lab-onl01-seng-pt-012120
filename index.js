6711245e2aa636ae33f391e47dc12941b30bd846
javers <joeychavira2016@gmail.com>
2020-08-12 16:06:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, string) {
+  return arr.filter( name =>
+    name.toLowerCase() === string.toLowerCase()
+  )
+}
+
+function fuzzyMatch(arr, string) {
+  return arr.filter( name =>
+    name.toLowerCase().indexOf(string.toLowerCase()) === 0
+  )
+}
+
+function matchName(arr, string) {
+  return arr.filter( record => record.name === string)
+}
